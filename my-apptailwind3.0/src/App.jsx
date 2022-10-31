1b4de172df1cfa06d3170e55f6574a3a426c9327
Soulholy <56435121+Soulholy@users.noreply.github.com>
2022-10-31 19:09:36 +0700

    Tailwind With Vite

---
+++ my-apptailwind3.0/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './style'
+
+export default function App() {
+  return (
+    <div className='bg-slate-600 w-full overflow-hidden'>
+
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          Navbar
+        </div>
+      </div>
+
+      <div>
+        
+      </div>
+
+
+      
+
+    </div>
+  )
+}
