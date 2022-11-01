bc0ee01a6286a2d89b2ce219a9bae22cb8b6fe56
Soulholy <56435121+Soulholy@users.noreply.github.com>
2022-11-01 19:08:03 +0700

    navbarSection

--- my-apptailwind3.0/src/App.jsx
+++ my-apptailwind3.0/src/App.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import styles from './style'
 
+// import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components/index";
+import Navbar from "./components/Navbar";
+
+
+
 export default function App() {
   return (
-    <div className='bg-slate-600 w-full overflow-hidden'>
-
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          Navbar
-        </div>
+    <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar /> 
       </div>
-
-      <div>
-        
-      </div>
-
-
-      
-
     </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Hero /> */}
+      </div>
+    </div>
+    
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        {/* <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer /> */}
+      </div>
+    </div>
+  </div>
   )
 }
---
+++ my-apptailwind3.0/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billing from "./billing";
+import CardDeal from "./cardDeal";
+import Business from "./bussiness";
+import Clients from "./clients";
+import CTA from "./CTA";
+import Stats from "./Stat";
+import Footer from "./Footer";
+import Testimonials from "./Testimonial";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
